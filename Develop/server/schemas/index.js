9e8e2dc6dcde2e06e7e39b4b7d73b6299f5c9c39
Nick Sprague <n.sprague18@gmail.com>
2024-06-14 16:28:48 -0400

    added schemas

---
+++ Develop/server/schemas/index.js
@@ -0,0 +1,52 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('./models'); 
+const resolvers = {
+  Query: {
+    
+    me: async (_, __, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate('savedBooks');
+        return user;
+      }
+      throw new AuthenticationError('You need to be logged in.');
+    },
+  },
+  Mutation: {
+    //"login" 
+    login: async (_, { email, password }) => {
+      // authentication token
+      
+    },
+    // "addUser" 
+    addUser: async (_, args) => {
+    
+    },
+    
+    saveBook: async (_, { bookData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          context.user._id,
+          { $push: { savedBooks: bookData } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in.');
+    },
+    
+    removeBook: async (_, { bookId }, context) => {
+      //user authentication
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          context.user._id,
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in.');
+    },
+  },
+};
+
+module.exports = resolvers;
